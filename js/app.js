cae376ca340ad7bf6ec3d1b24ea138ecd551b962
pzc <412726700@qq.com>
2015-07-09 19:08:37 +0800

    美化页面

--- js/app.js
+++ js/app.js
@@ -14,20 +14,29 @@ $('#search').on('click', function () {
   var playerNames = $('#nick').val().split(/\n|\r|\r\n/);
 
   Promise.map(playerNames, function (playerName) {
-    var obj = {serverName: serverName, playerName:playerName.indexOf('加入房间')!==-1?playerName.substring(0,playerName.length-5):playerName};
+    var obj = {
+      serverName: serverName,
+      playerName: playerName.indexOf('加入房间') !== -1 ? playerName.substring(0, playerName.length - 5) : playerName
+    };
     request(BASEURL + querystring.stringify(obj), function (err, response, body) {
       var $$ = cheerio.load(body, {
         normalizeWhitespace: true,
         xmlMode            : true
       });
       var recent = $$.html('.recent table');
-      var playerName = '<h3 class="player-name">' + $$('#playerNameLink').attr('title') + '</h3>';
+      recent = recent ? recent : '暂无数据';
 
+      var playerName = '<h5 class="player-name">' + obj.playerName + '</h5>';
       $('#data').append('<div class="recent-info">' + playerName + recent + '</div>');
     })
+  }).then(function(result){
+
+    $('#data').append(result);
   });
 });
 
-$('#clear').on('click',function(){
+$('#clear').on('click', function () {
   $('#data').html('')
 });
+
+$('select').material_select();
